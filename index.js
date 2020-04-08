a95e21b21e07f889840a6300f7c22b2b483b50bb
ylgr <tu.nguyen@midasprotocol.io>
2020-04-08 20:53:49 +0700

    (+) binance margin create, cancel, load, repay

--- index.js
+++ index.js
@@ -24,11 +24,43 @@ const headquartersSpreadsheetRangeIndex = {
       await binance.loadMarkets ();
       // const marginBalance = await binance.sapiGetMarginAccount();
       // console.log(marginBalance)
-      const allAssets = await binance.sapiGetMarginMaxBorrowable({"asset":"XRP"});
-      console.log(allAssets)
-      if(accountKeyInfo[3] === 'TuNV') {
-
+      // const allAssets = await binance.sapiGetMarginMaxBorrowable({"asset":"XRP"});
+      // console.log(allAssets)
+      if(accountKeyInfo[3] === 'TuNV') { // write balance information to sheet
       }
+
+      // console.log('Test dat lenh') // WORK
+      // const d = await binance.sapiPostMarginOrder({
+      //   symbol: 'BTCUSDT',
+      //   side: 'BUY',
+      //   type: 'LIMIT',
+      //   quantity: '0.01',
+      //   price: '3281',
+      //   timeInForce: 'GTC'
+      // })
+      // console.log(d)
+
+      // console.log('Test xoa lenh') // WORK
+      // const d = await binance.sapiDeleteMarginOrder({
+      //   symbol: 'BTCUSDT',
+      //   orderId: 1771090118
+      // })
+      // console.log(d)
+
+      // console.log('Test vay tien san') // WORK
+      // const d = await binance.sapiPostMarginLoan({
+      //   asset: 'USDT',
+      //   amount: 1
+      // })
+      // console.log(d)
+
+      // console.log('Test tra tien san') // WORK
+      // const d = await binance.sapiPostMarginRepay({
+      //   asset: 'BNB',
+      //   amount: 0.00000187
+      // })
+      // console.log(d)
+
       // console.log(balance.info.balances.filter(balance => balance.free > 0))
     }
   } catch (err) {
